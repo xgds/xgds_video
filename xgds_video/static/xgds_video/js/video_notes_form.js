80ab35be7283546116ecf71c445cff18961cbfe9
Tamar Cohen <tamar.e.cohen@nasa.gov>
2013-09-19 20:53:14 -0700

    work in progress note taking from video

--- xgds_video/static/xgds_video/js/video_notes_form.js
+++ xgds_video/static/xgds_video/js/video_notes_form.js
@@ -1,5 +1,37 @@
 
 /*
+ * Form submission
+ * 
+ */
+  $(function() {
+    $(".noteSubmit").click(function() {
+      // validate and process form here
+    	var content = $("input#id_content").val();
+    	var label = $("input#id_label").val();
+    	var tags = $("input#id_tags").val();
+    	
+    	 if (content == "") {
+    		 $("input#content").focus();
+    		 return false;
+    	 }
+    	var dataString = 'content='+ content + '&label=' + label + '&tags=' + tags;  
+    	//alert (dataString);return false;  
+    	$.ajax({  
+    	  type: "POST",  
+    	  url: submitNoteUrl,  
+    	  data: dataString,  
+    	  success: function() {  
+    		  alert("you are the WINNER!")
+    		  $("input#content").reset();
+    		  $("input#label").reset();
+    		  $("input#tags").reset();
+    	  }  
+    	});  
+    });
+  });
+  
+
+/*
 * Fancy tag input
 */
 
