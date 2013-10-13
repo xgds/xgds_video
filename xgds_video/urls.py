80c58adbb52fb3cf890c757ca18194e46927aa38
Trey Smith <trey.smith@nasa.gov>
2013-10-12 17:37:40 -0700

    fix for Django 1.5

--- xgds_video/urls.py
+++ xgds_video/urls.py
@@ -10,7 +10,3 @@ urlpatterns = patterns(
     (r'liveVideoFeed/(?P<feedName>\w+)$', views.liveVideoFeed, {}, 'xgds_video_live'),
     (r'recorded/$',views.displayEpisodeRecordedVideo, {}, 'xgds_video_recorded'),
 )
-
-
-#    (r'^$', 'django.views.generic.simple.redirect_to',
-#     {'url': settings.SCRIPT_NAME + 'plrpExplorer/'}),
