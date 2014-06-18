1a097abee196ea890dd68d81151728edd61d46b1
Grace <grace.lee@nasa.gov>
2014-06-17 20:55:05 -0700

    added url for recorded video overall

--- xgds_video/urls.py
+++ xgds_video/urls.py
@@ -9,5 +9,6 @@ urlpatterns = patterns(
     (r'liveVideoFeed/(?P<feedName>\w+)$', views.liveVideoFeed, {}, 'xgds_video_live'),
     (r'recorded/$', views.displayRecordedVideo, {}, 'xgds_video_recorded'),
     (r'videoIndexFile/(?P<flightAndSource>[^/]+)/(?P<segmentNumber>[^/]+)/prog_index.m3u8', views.videoIndexFile, {}, 'xgds_video_index_file'),
-    (r'^noteVideo2013/(?P<flightName>\w+)/(?P<time>[^/]+)/$', views.displayRecordedVideo, {}, 'noteVideo2013')
+    (r'^noteVideo2013/(?P<flightName>\w+)/(?P<time>[^/]+)/$', views.displayRecordedVideo, {}, 'noteVideo2013'),
+    (r'^recorded/(?P<flightName>\w+)/$', views.displayRecordedVideo, {}, 'flight_video_recorded')
 )
