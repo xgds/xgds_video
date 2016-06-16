1bc3ec44159438d6aac0ffdbaec6f3071b2519f3
Tamar Cohen <tamar.cohen@nasa.gov>
2016-06-16 14:15:17 -0600

    removed comments

--- xgds_video/static/xgds_video/js/packem.js
+++ xgds_video/static/xgds_video/js/packem.js
@@ -56,23 +56,3 @@
           }, 100 );
     });
     }
-    /*
-  // get item elements, jQuery-ify them
-  var $itemElems = $( $container.packery('getItemElements') );
-  // make item elements draggable
-  $itemElems.draggable().resizable();
-  // bind Draggable events to Packery
-  $container.packery( 'bindUIDraggableEvents', $itemElems );
-  
-  // handle resizing
-  var resizeTimeout;
-  $itemElems.on( 'resize', function( event, ui ) {
-    // debounce
-    if ( resizeTimeout ) {
-      clearTimeout( resizeTimeout );
-    }
-  
-    resizeTimeout = setTimeout( function() {
-      $container.packery( 'fit', ui.element[0] );
-    }, 100 );
-  });*/
