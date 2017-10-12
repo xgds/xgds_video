af1a21103e6a7715dfd7ec5adac816041e292b6e
Tamar Cohen <tamar.cohen@nasa.gov>
2017-10-12 12:01:05 -0700

    updated rest urls

--- xgds_video/urls.py
+++ xgds_video/urls.py
@@ -14,33 +14,35 @@
 # specific language governing permissions and limitations under the License.
 #__END_LICENSE__
 
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.views.generic.base import RedirectView, TemplateView
 
 from xgds_video import views, recordingUtil
 
-urlpatterns = [
-    url(r'^liveVideo/$', views.displayLiveVideo, {'loginRequired': False}, 'xgds_video_live'),  # active live undelayed video
-    url(r'^liveVideo/(?P<sourceShortName>[^/]+)/$', views.displayLiveVideo, {'loginRequired': False}, 'xgds_video_live_source'),  # active live undelayed video
-    url(r'videoIndexFile/(?P<flightName>[^/]+)/(?P<sourceShortName>[^/]+)/(?P<segmentNumber>[^/]+)/\w*\.m3u8', views.videoIndexFile, {'loginRequired': False}, 'xgds_video_index_file'),
-    url(r'^noteVideo/(?P<flightName>\w+)/(?P<time>[^/]+)/$', views.displayRecordedVideo, {}, 'xgds_video_recorded_time'),  # recorded video for one note
-    url(r'^noteVideo/(?P<flightName>\w+)/(?P<sourceShortName>[^/]+)/(?P<time>[^/]+)/$', views.displayRecordedVideo, {}, 'xgds_video_recorded_time'),  # recorded video for one note
-    # full-size still from video
-    url(r'^videoStillViewer/(?P<flightName>\w+)/(?P<time>[^/]+)/$', views.showStillViewerWindow, {},'videoStillViewer'), 
-    # for AJAX-y pages where params are built later
-    url(r'^videoStillViewer/$', views.showStillViewerWindow, {}, 'videoStillViewer'),
-    url(r'^videoStill/(?P<flightName>\w+)/(?P<time>[^/]+).thumbnail.jpg/$', views.displayVideoStillThumb, {}, 'videoStillThumb'),  # still thumbnail
-    url(r'^videoStill/(?P<flightName>\w+)/(?P<time>[^/]+).jpg/$', views.displayVideoStill, {}, 'videoStill'),  # full-size still from video
-    url(r'^videoStill/(?P<flightName>\w+)/(?P<time>[^/]+).jpg/(?P<isDownload>\d)/$', views.displayVideoStill, {}, 'xgds_video_downloadStill'),  # full-size still from video
-    url(r'^recorded/$', views.displayRecordedVideo, {'loginRequired': False}, 'xgds_video_recorded'),  # active live recorded video
-    url(r'^recorded/(?P<flightName>\w+)/$', views.displayRecordedVideo, {'loginRequired': False}, 'xgds_video_recorded'),  # active live recorded video
-    url(r'^recorded/(?P<flightName>\w+)/(?P<sourceShortName>\w+)/$', views.displayRecordedVideo, {'loginRequired': False}, 'xgds_video_recorded'),  # active live recorded video
-    url(r'^liveImageStream/$', views.liveImageStream, {'loginRequired': False}, 'live_image_stream'),  # shows image stream from rover.
-    url(r'^test/$', views.test, {}, 'test'),  # debug only
-    url(r'^recorded/(?P<flightName>\w+).json$', views.getEpisodeSegmentsJson, {'loginRequired': False}, 'xgds_video_recorded_json'),  # active recorded video json
-    url(r'^recorded/(?P<flightName>\w+)/(?P<sourceShortName>\w+).json$', views.getEpisodeSegmentsJson, {'loginRequired': False}, 'xgds_video_recorded_json'),  # active recorded video json
-    url(r'^stopRecording/(?P<flightName>\w+)', recordingUtil.stopFlightRecording, {'loginRequired': False}, 'xgds_video_stop_recording'), 
-    url(r'^startRecording/(?P<flightName>\w+)', recordingUtil.startFlightRecording, {'loginRequired': False}, 'xgds_video_start_recording'),
-    url(r'^testHLS$', TemplateView.as_view(template_name='xgds_video/testHLS.html'), {}, 'testHLS'),
-                
-    ]
+urlpatterns = [url(r'^liveVideo/$', views.displayLiveVideo, {}, 'xgds_video_live'),  # active live undelayed video
+               url(r'^liveVideo/(?P<sourceShortName>[^/]+)/$', views.displayLiveVideo, {}, 'xgds_video_live_source'),  # active live undelayed video
+               url(r'videoIndexFile/(?P<flightName>[^/]+)/(?P<sourceShortName>[^/]+)/(?P<segmentNumber>[^/]+)/\w*\.m3u8', views.videoIndexFile, {}, 'xgds_video_index_file'),
+               url(r'^noteVideo/(?P<flightName>\w+)/(?P<time>[^/]+)/$', views.displayRecordedVideo, {}, 'xgds_video_recorded_time'),  # recorded video for one note
+               url(r'^noteVideo/(?P<flightName>\w+)/(?P<sourceShortName>[^/]+)/(?P<time>[^/]+)/$', views.displayRecordedVideo, {}, 'xgds_video_recorded_time'),  # recorded video for one note
+               
+               # full-size still from video
+               url(r'^videoStillViewer/(?P<flightName>\w+)/(?P<time>[^/]+)/$', views.showStillViewerWindow, {},'videoStillViewer'), 
+               # for AJAX-y pages where params are built later
+               url(r'^videoStillViewer/$', views.showStillViewerWindow, {}, 'videoStillViewer'),
+               
+               url(r'^recorded/$', views.displayRecordedVideo, {}, 'xgds_video_recorded'),  # active live recorded video
+               url(r'^recorded/(?P<flightName>\w+)/$', views.displayRecordedVideo, {}, 'xgds_video_recorded'),  # active live recorded video
+               url(r'^recorded/(?P<flightName>\w+)/(?P<sourceShortName>\w+)/$', views.displayRecordedVideo, {}, 'xgds_video_recorded'),  # active live recorded video
+               
+               url(r'^liveImageStream/$', views.liveImageStream, {}, 'live_image_stream'),  # shows image stream from rover.
+    
+               url(r'^test/$', views.test, {}, 'test'),  # debug only
+               url(r'^testHLS$', TemplateView.as_view(template_name='xgds_video/testHLS.html'), {}, 'testHLS'),
+
+               url(r'^stopRecording/(?P<flightName>\w+)', recordingUtil.stopFlightRecording, {}, 'xgds_video_stop_recording'), 
+               url(r'^startRecording/(?P<flightName>\w+)', recordingUtil.startFlightRecording, {}, 'xgds_video_start_recording'),
+    
+               # Including these in this order ensures that reverse will return the non-rest urls for use in our server
+               url(r'^rest/', include('xgds_video.restUrls')),
+               url('', include('xgds_video.restUrls')),
+               ]
