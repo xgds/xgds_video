0b0b87c3ae69bce6a8c5fe20112a16f37251490b
DW Wheeler <dw.wheeler@nasa.gov>
2018-10-25 12:01:18 -0700

    apparently python doesn't need these parentheses

--- xgds_video/experimental.py
+++ xgds_video/experimental.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
+# __BEGIN_LICENSE__
+# Copyright (c) 2015, United States Government, as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# All rights reserved.
+#
+# The xGDS platform is licensed under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+# __END_LICENSE__
 
 import ffmpeg
 import argparse
@@ -41,15 +56,15 @@ def calculate_ts_file(folder_name, s_int):
 
 def hms_to_total_s(hms_string):
     tokens = hms_string.split(':')
-    if(len(tokens) == 3):
+    if len(tokens) == 3:
         h = tokens[0]
         m = tokens[1]
         s = tokens[2]
-    elif(len(tokens) == 2):
+    elif len(tokens) == 2 :
         h = 0
         m = tokens[0]
         s = tokens[1]
-    elif(len(tokens) == 1):
+    elif len(tokens) == 1 :
         h = 0
         m = 0
         s = tokens[0]
