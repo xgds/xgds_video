e46ff05930c97e77eadea841fe4412fb5fa37a7b
DW Wheeler <dw.wheeler@nasa.gov>
2018-10-24 17:52:09 -0700

    adding support for HH:mm:ss

--- xgds_video/experimental.py
+++ xgds_video/experimental.py
@@ -4,6 +4,7 @@ import ffmpeg
 import argparse
 import m3u8
 import os
+import string
 
 def take_screenshot(input_file, seconds_into, output_name):
     {
@@ -14,11 +15,6 @@ def take_screenshot(input_file, seconds_into, output_name):
         .run()
     }
 
-def screenshotify(input_file, max_seconds, out_folder):
-    for s in range(1,max_seconds):
-        next_string = os.path.join(out_folder, "out" + str(s) + ".png")
-        take_screenshot(input_file, s, next_string)
-    
 def calculate_ts_file(folder_name, s_int):
     # open the prog_index.m3u8
     m3u8_obj = m3u8.load(os.path.join(folder_name, 'prog_index.m3u8'))
@@ -29,34 +25,55 @@ def calculate_ts_file(folder_name, s_int):
     file_number = 0
     for seg_num in range(0, num_segs):
         next_delta = m3u8_obj.segments[seg_num].duration
-
         if acc_time + next_delta > float(s_float):
-            # save previous file number
-            file_number = seg_num - 1
+            # save file number
+            # (if you subtract 1, you're off by one. not sure why.)
+            file_number = seg_num
             break
-
         acc_time = acc_time + next_delta
 
-    return m3u8_obj.segments[seg_num].uri, s_float - acc_time
+    if file_number == num_segs:
+        print "HOUSTON WE HAVE A PROBLEM"
+        exit -1
+
+    return m3u8_obj.segments[file_number].uri, s_float - acc_time
+
+
+def hms_to_total_s(hms_string):
+    tokens = hms_string.split(':')
+    if(len(tokens) == 3):
+        h = tokens[0]
+        m = tokens[1]
+        s = tokens[2]
+    elif(len(tokens) == 2):
+        h = 0
+        m = tokens[0]
+        s = tokens[1]
+    elif(len(tokens) == 1):
+        h = 0
+        m = 0
+        s = tokens[0]
+    ans = int(s) + int(m)*60 + int(h)*60*60
+    return ans
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', help='video from which to take screenshot(s)', default='input.mov')
+    parser.add_argument('-f', help='video from which to take screenshot', default='input.mov')
     parser.add_argument('-ts', help='name of the folder containing ts files')
-    parser.add_argument('-ss', help='=s into video to take screenshot, or (many) stop taking screenshots', default='100')
-    parser.add_argument('-o', help='name of output file or (many) folder', default='output.png')
-    parser.add_argument('-many', help='take screenshots from 0 to ss seconds', action="store_true")
+    parser.add_argument('-hms', help='HH:mm:ss into video to take screenshot', default=2)
+    parser.add_argument('-o', help='name of output file', default='output.png')
 
     args, unknown = parser.parse_known_args()
     print args
     print unknown
 
+    seconds = hms_to_total_s(args.hms)
+
     if args.ts:
-        ts_file, offset = calculate_ts_file(args.ts, args.ss)
+        ts_file, offset = calculate_ts_file(args.ts, seconds)
         print 'ASKING FOR ' + os.path.join(args.ts, ts_file) + ' AT SECOND ' + str(int(offset))
+        # if you don't cast the offset time to an int picture is wavy gray
         take_screenshot(os.path.join(args.ts, ts_file), int(offset), args.o)
-    elif args.many:
-        screenshotify(args.f, int(args.ss), args.o)
     else:
-        take_screenshot(args.f, args.ss, args.o)
+        take_screenshot(args.f, seconds, args.o)
