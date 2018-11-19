4b34083c8685764778e824dd4c96274483d9ebde
Tamar Cohen <tamar.cohen@nasa.gov>
2018-11-19 10:57:21 -0800

    merged frame grab tests into tests.py

--- xgds_video/test/tests.py
+++ xgds_video/test/tests.py
@@ -28,6 +28,11 @@ class xgds_videoTest(TransactionTestCase):
     filepath = '/home/xgds/xgds_subsea/apps/xgds_video/test/test_files/na1.stream_2018-09-02-22.58.45.785-UTC_65'
     ref_2 = filepath + '/Screenshot_2018.09.02-22.58.52.png'
 
+    ts_1 = filepath + '/fileSequence0.ts'
+    ref_1 = filepath + '/Screenshot_2018.09.02-22.58.47.png'
+    ref_2 = filepath + '/Screenshot_2018.09.02-22.58.52.png'
+
+
     fixtures = ['geocamTrack_initial_data.json',
                 'xgds_core_initial_data.json',
                 'xgds_core_testing.json',
@@ -48,10 +53,59 @@ class xgds_videoTest(TransactionTestCase):
         # Restore the global variable we changed during setup
         settings.XGDS_CORE_DEFAULT_VEHICLE_PK = self.default_vehicle_pk
 
+    def test_take_screenshot(self):
+        bytes = take_screenshot(TestFrameGrab.ts_1, 2)
 
-    def test_xgds_video(self):
-        # print "testing git hook 7 in xgds_video"
-        pass
+        with open(TestFrameGrab.ref_1, 'rb') as f:
+            reference_bytes = f.read()
+            f.close()
+
+        # doing this so it does not print obnoxious binary
+        # if the test fails
+        equals_reference = (bytes == reference_bytes)
+        self.assertTrue(equals_reference)
+
+    def test_grab_frame(self):
+        # path, start, grab
+        bytes = grab_frame(TestFrameGrab.filepath,
+                           dateparser('20180902 22:58:45'),
+                           dateparser('20180902 22:58:52'))
+
+        with open(TestFrameGrab.ref_2, 'rb') as f:
+            reference_bytes_2 = f.read()
+            f.close()
+
+        equals_reference = (bytes == reference_bytes_2)
+        self.assertTrue(equals_reference)
+
+        # path, hms
+        bytes = grab_frame(TestFrameGrab.filepath,
+                           hms='00:00:07')
+
+        equals_reference = (bytes == reference_bytes_2)
+        self.assertTrue(equals_reference)
+
+        # file, hms
+        bytes = grab_frame(file=TestFrameGrab.ts_1,
+                           hms='00:00:02')
+
+        with open(TestFrameGrab.ref_1, 'rb') as f:
+            reference_bytes_1 = f.read()
+            f.close()
+
+        equals_reference = (bytes == reference_bytes_1)
+        self.assertTrue(equals_reference)
+
+    def test_exceptions(self):
+        # path, grab, no start
+        with self.assertRaises(Exception):
+            bytes = grab_frame(TestFrameGrab.filepath,
+                            grab_time=dateparser('20180902 22:58:52'))
+
+        # no grab, no start
+        with self.assertRaises(Exception):
+            bytes = grab_frame(TestFrameGrab.filepath)
+
 
     def test_frame_grab_and_insert_database(self):
         """
